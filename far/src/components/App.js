a0d91dcac2613ad1197138cb772a2363cde427a0
Haya <haya.ehab.dev@gmail.com>
2025-07-27 16:33:06 +0300

    Modified: far/src/App.js

---
+++ far/src/components/App.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Logo from "./logo";
+import Form from "./Form";
+import PackingList  from "./PackingList";
+import { Stats } from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm("Are your sure you wanna delete everything");
+
+    if (confirmed) setItems([])
+  }
+ 
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAdddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+
+  
+
+
+
